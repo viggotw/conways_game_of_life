7e4a3459192a3632153c5bc2fe480a68598a96d6
ViggoTW <viggotw@gmail.com>
2023-04-04 19:27:27 +0200

    Add painting functionality

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
+let painting = false;
+let paintColor = "black";
+
 window.addEventListener('load', function () {
-    var defaultSize = 5; // set your default grid size here
+    let defaultSize = 10; // set your default grid size here
     document.getElementById("size").value = defaultSize;
     createGrid();
 });
 
 function createGrid() {
-    var size = document.getElementById("size").value;
-    var grid = document.getElementById("grid");
+    let size = document.getElementById("size").value;
+    let grid = document.getElementById("grid");
     grid.innerHTML = "";
-    for (var i = 0; i < size; i++) {
-        var row = document.createElement("tr");
-        for (var j = 0; j < size; j++) {
-            var cell = document.createElement("td");
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("tr");
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement("td");
             cell.setAttribute("data-row", i);
             cell.setAttribute("data-col", j);
+            cell.classList.add("cell");
             cell.addEventListener("click", toggleColor);
+            cell.addEventListener("mousedown", startPainting);
+            cell.addEventListener("mouseup", stopPainting);
             row.appendChild(cell);
         }
         grid.appendChild(row);
     }
+
+    // Add event listener to the document object for the "mousemove" event
+    document.addEventListener("mousemove", function (event) {
+        // Check if the "painting" flag is set
+        if (painting) {
+            // Get the cell that the mouse is currently over
+            const target = event.target;
+            // Check if the cell is a grid cell (i.e., has the "cell" class)
+            if (target.classList.contains("cell")) {
+                console.log("mousemove+painting+cell")
+                // Set the background color of the cell to the stored color
+                target.style.backgroundColor = paintColor;
+            }
+        }
+    });
+}
+
+function startPainting(event) {
+    painting = true;
+    paintColor = cell.style.backgroundColor;
+    toggleColor(event);
+}
+
+function stopPainting() {
+    painting = false;
 }
 
 function toggleColor(event) {
-    var cell = event.target;
-    var currentColor = cell.style.backgroundColor;
+    let cell = event.target;
+    let currentColor = cell.style.backgroundColor;
     if (!currentColor || currentColor === "white") {
         cell.style.backgroundColor = "black";
     } else {
