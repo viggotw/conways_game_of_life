ac589a8ed78368e9b367765eb4886285e1318d60
ViggoTW <viggotw@gmail.com>
2023-04-09 21:44:17 +0200

    Update paint logic and design

--- script.js
+++ script.js
@@ -11,45 +11,52 @@ function createGrid() {
     let size = document.getElementById("size").value;
     let grid = document.getElementById("grid");
     grid.innerHTML = "";
+    grid.style.setProperty('--size', size); 
     for (let i = 0; i < size; i++) {
-        let row = document.createElement("tr");
+        let row = document.createElement("div");
         for (let j = 0; j < size; j++) {
-            let cell = document.createElement("td");
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
             cell.setAttribute("data-row", i);
             cell.setAttribute("data-col", j);
-            cell.classList.add("cell");
-            cell.addEventListener("click", toggleColor);
+            // cell.addEventListener("click", toggleColor);
             cell.addEventListener("mousedown", startPainting);
             cell.addEventListener("mouseup", stopPainting);
+            // cell.addEventListener("mouseover", applyPainting);
             row.appendChild(cell);
         }
-        grid.appendChild(row);
+        grid.appendChild(row);  
     }
-
-    // Add event listener to the document object for the "mousemove" event
-    document.addEventListener("mousemove", function (event) {
-        // Check if the "painting" flag is set
-        if (painting) {
-            // Get the cell that the mouse is currently over
-            const target = event.target;
-            // Check if the cell is a grid cell (i.e., has the "cell" class)
-            if (target.classList.contains("cell")) {
-                console.log("mousemove+painting+cell")
-                // Set the background color of the cell to the stored color
-                target.style.backgroundColor = paintColor;
-            }
-        }
-    });
+    grid.addEventListener("mouseover", applyPainting);
 }
 
 function startPainting(event) {
+    console.log("start painting");
+    let cell = event.target;
     painting = true;
-    paintColor = cell.style.backgroundColor;
     toggleColor(event);
+    paintColor = cell.style.backgroundColor;
+    console.log(painting);
+    console.log(paintColor);
+    event.target.removeEventListener("mouseover", applyPainting);
 }
 
 function stopPainting() {
+    console.log("stop painting");
+    console.log(painting);
+    console.log(paintColor);
     painting = false;
+    event.target.addEventListener("mouseover", applyPainting);
+}
+
+function applyPainting(event) {
+    let cell = event.target;
+    if (painting && cell.classList.contains("cell")) {
+        console.log("apply painting");
+        console.log(painting);
+        console.log(paintColor);
+        cell.style.backgroundColor = paintColor;
+    }
 }
 
 function toggleColor(event) {
